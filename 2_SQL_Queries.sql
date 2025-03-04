4dd02f4132e2a7999691e8013519793de0ac7627
Naman Gupta <113845871+naman02602@users.noreply.github.com>
2025-03-03 21:29:41 -0500

    Update 2_SQL_Queries.sql

--- 2_SQL_Queries.sql
+++ 2_SQL_Queries.sql
@@ -17,10 +17,11 @@ ORDER BY receipt_count DESC;
 
 -- 2. How does the ranking of the top 5 brands by receipts scanned for the recent month compare to the ranking for the previous month?
 WITH current_month AS (
-    SELECT r.receipt_id, ri.barcode
+    SELECT r.receipt_id, ri.barcode, ib.brand_id
     FROM Receipts r
     JOIN ReceiptItems ri ON r.receipt_id = ri.receipt_id
-    WHERE r.purchase_date >= DATEADD(MONTH, -1, GETDATE())
+	JOIN Item_Brand ib ON ri.barcode = ib.barcode
+    WHERE r.purchase_date > EOMONTH(GETDATE(), -1) 
 ),
 
 current_month_ranking AS (
@@ -28,16 +29,17 @@ current_month_ranking AS (
            COUNT(DISTINCT cm.receipt_id) AS receipt_count, 
            RANK() OVER (ORDER BY COUNT(DISTINCT cm.receipt_id) DESC) AS rank
     FROM current_month cm
-    JOIN Brands b ON cm.barcode = b.barcode
+    JOIN Brands b ON cm.brand_id = b.brand_id
     GROUP BY b.name
 ),
 
 previous_month AS (
-    SELECT r.receipt_id, ri.barcode
+    SELECT r.receipt_id, ri.barcode, ib.brand_id
     FROM Receipts r
     JOIN ReceiptItems ri ON r.receipt_id = ri.receipt_id
-    WHERE r.purchase_date >= DATEADD(MONTH, -2, GETDATE()) 
-          AND r.purchase_date < DATEADD(MONTH, -1, GETDATE())
+	JOIN Item_Brand ib ON ri.barcode = ib.barcode
+    WHERE r.purchase_date >= DATEADD(DAY, 1, EOMONTH(GETDATE(), -2)) 
+          AND r.purchase_date <= EOMONTH(GETDATE(), -1)
 ),
 
 previous_month_ranking AS (
@@ -45,12 +47,12 @@ previous_month_ranking AS (
            COUNT(DISTINCT pm.receipt_id) AS receipt_count, 
            RANK() OVER (ORDER BY COUNT(DISTINCT pm.receipt_id) DESC) AS rank
     FROM previous_month pm
-    JOIN Brands b ON pm.barcode = b.barcode
+    JOIN Brands b ON pm.brand_id = b.brand_id
     GROUP BY b.name
 )
 
 SELECT 
-    COALESCE(cmr.brand_name, pmr.brand_name) AS brand_name, 
+    TOP 5 COALESCE(cmr.brand_name, pmr.brand_name) AS brand_name, 
     cmr.receipt_count AS current_receipt_count,
     cmr.rank AS current_rank, 
     pmr.receipt_count AS previous_receipt_count, 
@@ -71,24 +73,20 @@ ORDER BY avg_spend DESC;
 
 
 -- 4. When considering total number of items purchased from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
-WITH items_comparison AS (
-    SELECT 
-        r.rewards_receipt_status, 
-        SUM(ri.quantity_purchased) AS total_items
-    FROM Receipts r
-    JOIN ReceiptItems ri ON r.receipt_id = ri.receipt_id
-    WHERE r.rewards_receipt_status IN ('Accepted', 'Rejected')
-    GROUP BY r.rewards_receipt_status
-)
-SELECT rewards_receipt_status, total_items
-FROM items_comparison
+SELECT 
+    r.rewards_receipt_status, 
+    SUM(ri.quantity_purchased) AS total_items
+FROM Receipts r
+JOIN ReceiptItems ri ON r.receipt_id = ri.receipt_id
+WHERE r.rewards_receipt_status IN ('Accepted', 'Rejected')
+GROUP BY r.rewards_receipt_status
 ORDER BY total_items DESC;
 
 -- 5. Which brand has the most spend among users who were created within the past 6 months?
 WITH recent_users AS (
     SELECT user_id
     FROM Users
-    WHERE created_date >= DATEADD(MONTH, -6, GETDATE())  -- Users created in the last 6 months
+    WHERE created_date >= DATEADD(DAY, 1, EOMONTH(GETDATE(), -6))  -- Users created in the last 6 months
 ),
 
 user_receipts AS (
@@ -119,7 +117,7 @@ ORDER BY bs.total_spend DESC;  -- Select the brand with the highest total spend
 WITH recent_users AS (
     SELECT user_id
     FROM Users
-    WHERE created_date >= DATEADD(MONTH, -6, GETDATE())  -- Get users created in the last 6 months
+    WHERE created_date >= DATEADD(DAY, 1, EOMONTH(GETDATE(), -6))  -- Get users created in the last 6 months
 ),
 
 user_receipts AS (
